00a08d1d1446bdb64c6efb92858a742705b32130
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2024-05-02 21:17:51 -0300

    Update Ejercicio4.cpp

--- Ejercicio4.cpp
+++ Ejercicio4.cpp
@@ -19,17 +19,21 @@ int potencia(int n, int m) {
     return potencia_aux(n, 0, m);
 }
 
-int montaña_aux( vector<int> v, int desde, int hasta) {
-    if ( v[desde] < v[desde + 1] && v[desde + 1] > v[desde + 2] ) {
-        return v[desde + 1];
-    }
-    else {
+
+int montaña_aux(vector<int> v, int desde, int hasta){
+    if (desde == hasta -1){
+        return v[desde];
+    }else{
         int medio = (desde + hasta) / 2;
-        return montaña_aux(v, medio-1, hasta);
+        if (montaña_aux(v, desde, medio) > montaña_aux(v, medio, hasta)){
+            return montaña_aux(v, desde, medio);
+        }else{
+            return montaña_aux(v, medio, hasta);
+        }
     }
 }
 
-int montaña(vector<int> v) {
+int montaña(vector<int> v){
     return montaña_aux(v, 0, v.size());
 }
 
@@ -37,8 +41,10 @@ int main() {
     int res_a = potencia(2, 4);
     cout << "La potencia es " << res_a << endl;
 
-    vector<int> b = {1, 2, 3, 4, 5, 6, 7, 2, 1};
+    vector<int> b = {1, 2, 10, 5, 4, 3, 2, 1};
     int res_b = montaña(b);
     cout << "El pico de la montaña es: " << res_b << endl;
 }
+
+
 
